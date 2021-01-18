d2898ebd602af26dc0f025d4062b28cb57c28d13
Nedheesh Hasija <nedheesh.hasija@gmail.com>
2021-01-18 16:36:34 +0530

    Permission handler included

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,26 +1,27 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:location_permissions/location_permissions.dart';
 
 class Location {
   Location({this.latitude, this.longitude});
 
   double latitude;
   double longitude;
-  var geolocator = Geolocator();
 
   Future<void> getCurrentLocation() async {
     try {
-      GeolocationStatus permission = await Geolocator().checkGeolocationPermissionStatus();
-      if (permission == GeolocationStatus.denied) {
-        ;
+      PermissionStatus permission =
+          await LocationPermissions().checkPermissionStatus();
+      if (permission != PermissionStatus.granted) {
+        permission = await LocationPermissions().requestPermissions();
       }
+
       Position position = await Geolocator().getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low,
       );
 
       latitude = position.latitude;
       longitude = position.longitude;
-    }
-    catch(e) {
+    } catch (e) {
       print(e);
     }
   }
